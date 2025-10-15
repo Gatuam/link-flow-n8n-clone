c962d70a5ec75263857c6929d102447e80353894
EK GAUTHAM <sggtm42@gmail.com>
2025-10-15 13:01:26 +0000

    test: llm ai bg job test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import prisma from "@/db/prisma";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 
 export default function Home() {
-  
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const textAi = useMutation(trpc.testAi.mutationOptions());
+  console.log(textAi);
+
   return (
-   <div>
-    hi
-   </div>
+    <div>
+      <Button disabled={textAi.isPending} onClick={() => textAi.mutate()}>
+        Test ai
+      </Button>
+      hi
+    </div>
   );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,21 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+import { inngest } from "@/inngest/client";
+
 export const appRouter = createTRPCRouter({
+  testAi: baseProcedure.mutation(async () => {
+  const data =  await inngest.send({
+      name : "execute/ai"
+    })
+    return data
+  }),
   hello: baseProcedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
       return {
